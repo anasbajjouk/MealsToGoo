655ba335c02ca0fef3e40db4def71a74ded195ed
Anas Bajjouk <bajjouk1@gmail.com>
2021-05-19 16:11:12 +0200

    Added theme and some refarctoring of restaurants components

---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Searchbar } from "react-native-paper";
+import { StatusBar, SafeAreaView, StyleSheet, View } from "react-native";
+import RestaurantInfoCard from "../../../components/restaurant-info-card.component";
+import styled from "styled-components";
+
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  margin-top: ${StatusBar.currentHeight}px;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`}
+`;
+
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+const RestaurantListContainer = styled.View`
+  background-color: blue;
+  padding: ${(props) => props.theme.space[3]};
+  flex: 1;
+`;
+
+const RestaurantsScreen = () => {
+  return (
+    <SafeArea>
+      <SearchContainer>
+        <Searchbar />
+      </SearchContainer>
+
+      <RestaurantListContainer>
+        <RestaurantInfoCard />
+      </RestaurantListContainer>
+    </SafeArea>
+  );
+};
+
+export default RestaurantsScreen;
